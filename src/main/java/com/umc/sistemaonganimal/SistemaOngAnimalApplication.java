8f631d953564ab37164467a8a9477f2c8fea59a6
Ana <anaraquel.dsc@gmail.com>
2025-10-01 11:43:24 -0300

    chore: adiciona a configuração inicial do projeto Spring Boot

---
+++ src/main/java/com/umc/sistemaonganimal/SistemaOngAnimalApplication.java
@@ -0,0 +1,13 @@
+package com.umc.sistemaonganimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaOngAnimalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaOngAnimalApplication.class, args);
+    }
+
+}
