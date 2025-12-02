0d7b1bac29024ac3bb7056c4b17f710fe4e9d621
Ana <anaraquel.dsc@gmail.com>
2025-12-02 14:57:05 -0300

    fix: Corrige as anotações faltantes do lombok na classe Animal

--- src/main/java/com/umc/sistemaonganimal/domain/model/Animal.java
+++ src/main/java/com/umc/sistemaonganimal/domain/model/Animal.java
@@ -1,10 +1,13 @@
 package com.umc.sistemaonganimal.domain.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.time.LocalDate;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "animal")
 public class Animal {
