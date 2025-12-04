5f926b0519e3a198a0c772a84f1a3d9ae18e83b5
Nicolas Mariano de Azevedo <azevedo.nicolas05@gmail.com>
2025-12-04 15:24:38 -0300

    fix:adiciona restrições na tabela animal, nos atributos idade, castrado,dt_resgate e dt_saida

--- src/main/resources/db/migration/V001__cria_tabela_animal.sql
+++ src/main/resources/db/migration/V001__cria_tabela_animal.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS animal (
 	id SERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
-    idade INT,
+    idade INT CHECK (idade >= 0),
     sexo VARCHAR(10) NOT NULL CHECK (sexo IN ('MACHO', 'FEMEA')),
     porte VARCHAR(10) NOT NULL CHECK (porte IN ('PEQUENO', 'MEDIO', 'GRANDE')),
-    castrado BOOLEAN,
-    dt_resgate DATE NOT NULL,
+    castrado BOOLEAN DEFAULT FALSE,
+    dt_resgate DATE NOT NULL NOT NULL CHECK (dt_resgate <= CURRENT_DATE),
     dt_saida DATE,
     cor_olhos VARCHAR(20),
     cor_pelagem VARCHAR(20),
-    observacao TEXT
+    observacao TEXT,
+    CHECK (dt_saida IS NULL OR dt_saida >= dt_resgate)
 );
