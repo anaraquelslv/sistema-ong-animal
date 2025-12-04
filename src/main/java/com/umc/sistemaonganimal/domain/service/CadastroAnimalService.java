d964ca956abfda3e1dd2f2f97d5a190ecf7148fd
Ana <anaraquel.dsc@gmail.com>
2025-12-04 12:53:56 -0300

    feat: adiciona métodos listarTodos e bucarPorId

--- src/main/java/com/umc/sistemaonganimal/domain/service/CadastroAnimalService.java
+++ src/main/java/com/umc/sistemaonganimal/domain/service/CadastroAnimalService.java
@@ -1,15 +1,28 @@
 package com.umc.sistemaonganimal.domain.service;
 
+import com.umc.sistemaonganimal.domain.exception.EntidadeNaoEncontradaException;
 import com.umc.sistemaonganimal.domain.model.Animal;
 import com.umc.sistemaonganimal.domain.repository.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CadastroAnimalService {
 
     @Autowired
     private AnimalRepository animalRepository;
+
+    public List<Animal> listarTodos(){
+        return animalRepository.findAll();
+    }
+
+    public Animal buscarPorId(Long id){
+        return animalRepository.findById(id)
+                .orElseThrow(() -> new EntidadeNaoEncontradaException("Animal não encontrado com o id" + id));
+    }
 
     public Animal salvar(Animal animal) {
         return animalRepository.save(animal);
