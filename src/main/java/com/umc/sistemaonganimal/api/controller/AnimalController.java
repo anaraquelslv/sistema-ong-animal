d1084a5198ab522d795a422bed270e87c6f10727
Ana <anaraquel.dsc@gmail.com>
2025-12-04 14:16:38 -0300

    feat: adiciona método buscar por id

--- src/main/java/com/umc/sistemaonganimal/api/controller/AnimalController.java
+++ src/main/java/com/umc/sistemaonganimal/api/controller/AnimalController.java
@@ -1,10 +1,10 @@
 package com.umc.sistemaonganimal.api.controller;
 
 import com.umc.sistemaonganimal.domain.model.Animal;
-import com.umc.sistemaonganimal.domain.repository.AnimalRepository;
 import com.umc.sistemaonganimal.domain.service.CadastroAnimalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +20,11 @@ public class AnimalController {
     @GetMapping
     public List<Animal> listar(){
         return animalService.listarTodos();
+    }
+
+    @GetMapping("/{animalId}")
+    public Animal buscarPorId(@PathVariable("animalId") Long id){
+        return animalService.buscarPorId(id);
     }
 
 
