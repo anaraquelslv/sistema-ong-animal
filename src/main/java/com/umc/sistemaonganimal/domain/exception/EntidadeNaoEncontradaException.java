a65f4f14b1aab2c427079b6fb6b7f26e57c0750a
Ana <anaraquel.dsc@gmail.com>
2025-12-04 15:51:25 -0300

    feat: adiciona anotação para que quando a exceção for lançada, o código de resposta HTTP seja NOT FOUND 404

--- src/main/java/com/umc/sistemaonganimal/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/umc/sistemaonganimal/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,5 +1,11 @@
 package com.umc.sistemaonganimal.domain.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// TODO Alterar esse tratamento de erro para utilizar Handler Global futuramente
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class EntidadeNaoEncontradaException extends RuntimeException {
     public EntidadeNaoEncontradaException(String message) {
         super(message);
