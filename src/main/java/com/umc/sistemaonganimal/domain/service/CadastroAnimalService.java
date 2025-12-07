b27ba570a9b17f3312c4aa97464fc98cc6ca54c8
Ana <anaraquel.dsc@gmail.com>
2025-12-07 14:32:09 -0300

    feat(CadastroAnimalService): cria método para excluir registro

--- src/main/java/com/umc/sistemaonganimal/domain/service/CadastroAnimalService.java
+++ src/main/java/com/umc/sistemaonganimal/domain/service/CadastroAnimalService.java
@@ -1,9 +1,11 @@
 package com.umc.sistemaonganimal.domain.service;
 
+import com.umc.sistemaonganimal.domain.exception.EntidadeEmUsoException;
 import com.umc.sistemaonganimal.domain.exception.EntidadeNaoEncontradaException;
 import com.umc.sistemaonganimal.domain.model.Animal;
 import com.umc.sistemaonganimal.domain.repository.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,10 +23,19 @@ public class CadastroAnimalService {
 
     public Animal buscarPorId(Long id){
         return animalRepository.findById(id)
-                .orElseThrow(() -> new EntidadeNaoEncontradaException("Animal não encontrado com o id: " + id));
+                .orElseThrow(() -> new EntidadeNaoEncontradaException(String.format("Não existe um registro de Animal com o id: %d", id)));
     }
 
     public Animal salvar(Animal animal) {
         return animalRepository.save(animal);
+    }
+
+    public void excluir(Long id) {
+        try {
+            Animal animalExcluir = buscarPorId(id);
+            animalRepository.delete(animalExcluir);
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(String.format("A entidade com id %d está em uso e não pode ser excluida", id));
+        }
     }
 }
