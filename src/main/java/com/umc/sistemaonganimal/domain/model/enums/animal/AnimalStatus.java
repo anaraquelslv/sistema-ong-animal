dbe6c8390af7f44e4eb43b7d210047fd979bb3cc
Ana <anaraquel.dsc@gmail.com>
2025-12-10 20:27:19 -0300

    feat: adiciona enums para especie e status do animal

---
+++ src/main/java/com/umc/sistemaonganimal/domain/model/enums/animal/AnimalStatus.java
@@ -0,0 +1,11 @@
+package com.umc.sistemaonganimal.domain.model.enums.animal;
+
+public enum AnimalStatus {
+    DISPONIVEL_PARA_ADOCAO,
+    EM_TRATAMENTO,
+    EM_QUARENTENA,
+    ADOTADO,
+    RECEM_RESGATADO,
+    FALECIDO
+
+}
