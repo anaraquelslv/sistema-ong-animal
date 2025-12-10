392e329079adc1c968ea2ea798ebbbc16841461a
Ana <anaraquel.dsc@gmail.com>
2025-12-10 20:55:09 -0300

    fix: corrige constraint de chave estrangeira da tabela animal

--- src/main/resources/db/migration/V008__adiciona_fk_animal_adotante.sql
+++ src/main/resources/db/migration/V008__adiciona_fk_animal_adotante.sql
@@ -1,6 +1,6 @@
 ALTER TABLE animal
-ADD COLUMN adotante_cpf VARCHAR(11);
+ADD COLUMN adotante_id INT;
 
 ALTER TABLE animal
 ADD CONSTRAINT fk_animal_adotante
-FOREIGN KEY (adotante_cpf) REFERENCES adotante(CPF);
+FOREIGN KEY (adotante_id) REFERENCES adotante(id);
