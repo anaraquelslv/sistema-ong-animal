39411b90f1187f19d76d8004d7425f7d84307488
Ana <anaraquel.dsc@gmail.com>
2025-12-13 17:32:17 -0300

    feat: cria exceção personalizada para Espécie não encontrada

---
+++ src/main/java/com/umc/sistemaonganimal/domain/exception/EspecieNotFoundException.java
@@ -0,0 +1,11 @@
+package com.umc.sistemaonganimal.domain.exception;
+
+public class EspecieNotFoundException extends EntityNotFoundException {
+    public EspecieNotFoundException(String message) {
+        super(message);
+    }
+
+    public EspecieNotFoundException(Long id) {
+        this(String.format(String.format("Não existe um registro de Especie com o id: %d", id)));
+    }
+}
